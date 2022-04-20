cdb6de5771acfd6c65cc616f49992a79eef73e86
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-20 17:26:27 +0300

    footer section added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from "./logo.svg"
 import "./App.css"
 import "bootstrap/dist/css/bootstrap.min.css"
+import MyFooter from "./Component/MyFooter"
 import MyNavbar from "./Component/MyNavbar"
 function App() {
   return (
     <div>
       <MyNavbar />
+      <MyFooter />
     </div>
   )
 }
--- src/Component/MyFooter.jsx
+++ src/Component/MyFooter.jsx
@@ -1,3 +1,8 @@
 const MyFooter = () => (
-
+  <footer id="sticky-footer" class="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div class="container text-center">
+      <small>Copyright &copy; Kenneth 2022</small>
+    </div>
+  </footer>
 )
+export default MyFooter

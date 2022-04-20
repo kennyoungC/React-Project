bb9c589b0916753f9d6309afdc38df074459266b
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-04-20 18:03:55 +0300

    Update LatestRelease.jsx
    prices added

--- src/Component/LatestRelease.jsx
+++ src/Component/LatestRelease.jsx
@@ -8,11 +8,12 @@ const LatestRelease = () => (
           <Card>
             <Card.Img variant="top" src={book.img} />
             <Card.Body>
-              <Card.Title>Card Title</Card.Title>
+              <Card.Title>{book.title}</Card.Title>
               <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the card's content.
               </Card.Text>
+              <Card.Text>{book.price}</Card.Text>
               <Button variant="primary">Go somewhere</Button>
             </Card.Body>
           </Card>
